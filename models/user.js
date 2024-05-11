54941dc23da02f416a00c000ac125d3087d98f30
KaixiangJony <kaixiangjohn@gmail.com>
2024-05-11 18:09:40 +0930

    update db request for orders
    update db request for orders

--- models/user.js
+++ models/user.js
@@ -34,7 +34,73 @@ class User {
     const db = getDb();
     return db
     .collection('user')
-    .updateOne({_id: new ObjectId(this._id)}, { $set: { cart: updatedCart } });
+    .updateOne(
+      {_id: new ObjectId(this._id)}, 
+      { $set: { cart: updatedCart } 
+    });
+  }
+
+  deleteItemFromCart(prodId){
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== prodId.toString();
+    });
+    const db = getDb();
+    return db
+    .collection('user')
+    .updateOne(
+      {_id: new ObjectId(this._id)}, 
+      { $set: { cart: {items: updatedCartItems} } 
+    });
+  }
+
+  getCart(){
+    const db = getDb();
+    const productsIds = this.cart.items.map(i => {
+      return i.productId;
+    })
+    return db.collection('products')
+    .find({ _id: {$in: productsIds } })
+    .toArray()
+    .then(products => {
+      return products.map(p => {
+        return {
+          ...p, 
+          quantity: this.cart.items.find(i => {
+          return i.productId.toString() === p._id.toString();
+        }).quantity
+      };
+      });
+    });
+  }
+
+  addOrder(){
+    const db = getDb();
+    return this.getCart()
+    .then(products => {
+      const order = {
+        items: products,
+        user: {
+          _id: new ObjectId(this._id),
+          name: this.name,
+          email: this.email
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    })
+    .then(result => {
+      this.cart = {items: []};
+      return db
+        .collection('user')
+        .updateOne(
+        {_id: new ObjectId(this._id)}, 
+        { $set: { cart: {items: []} } 
+    });
+    })
+  }
+
+  getOrders(){
+    const db = getDb();
+    return db.collection('orders').find({'user._id': new ObjectId(this._id)}).toArray();
   }
 
   static fetchById(userId){
