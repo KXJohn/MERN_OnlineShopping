e24169cf79590a54a81108f4189aa9b0ef7c0421
KaixiangJony <kaixiangjohn@gmail.com>
2024-05-26 16:46:47 +0930

    use async func to do request on client side

--- controllers/admin.js
+++ controllers/admin.js
@@ -174,8 +174,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
@@ -187,11 +187,10 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log("DESTROYED PRODUCT");
-      res.redirect("/admin/products");
+      res.status(200).json({ messgae: "Success" });
+      // res.redirect("/admin/products");
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ messgae: "Failed" });
     });
 };
