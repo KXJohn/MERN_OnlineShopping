f311e5b163eeb1f6ce44f8d7c745062f49ca3b46
KaixiangJony <kaixiangjohn@gmail.com>
2024-05-31 17:06:53 +0930

    Add server validation

--- platform/controllers/feed.js
+++ platform/controllers/feed.js
@@ -1,15 +1,43 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{title: 'Test', content: 'Test Content'}]
-    });
+  res.status(200).json({
+    posts: [
+      {
+        _id: 1,
+        title: "Test",
+        content: "Test Content",
+        imageUrl: "",
+        creator: {
+          name: "Jonny",
+          createdAt: new Date(),
+        },
+      },
+    ],
+  });
 };
 
 exports.createPost = (req, res, next) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    // Create post in db   
-    res.status(201).json({
-        message: 'Post created successfully',
-        post: { id: new Date().toISOString(), title, content }
-    });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({ message: "Validation failed", errors: errors.array() });
+  }
+
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: "Post created successfully",
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content: content,
+      creator: {
+        name: "Jonny",
+        createdAt: new Date(),
+      },
+    },
+  });
 };
